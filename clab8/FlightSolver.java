611b664f2c16ba45ad2d185765b17ef808238002
wenhao <1119356833@qq.com>
2020-09-19 22:03:22 -0700

    clap

--- clab8/FlightSolver.java
+++ clab8/FlightSolver.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.PriorityQueue;
 
 /**
  * Solver for the Flight problem (#9) from CS 61B Spring 2018 Midterm 2.
@@ -7,14 +9,42 @@ import java.util.ArrayList;
  * considered to be in the air at the same time.
  */
 public class FlightSolver {
+    private  int MaxNum = 0;
+
 
     public FlightSolver(ArrayList<Flight> flights) {
         /* FIX ME */
+        PriorityQueue<Flight> startMinPQ = new PriorityQueue<>(new Comparator<Flight>() {
+            @Override
+            public int compare(Flight flight1, Flight flight2) {
+                return Integer.compare(flight1.startTime(), flight2.startTime());
+            }
+        });
+
+        PriorityQueue<Flight> endMinPQ = new PriorityQueue<>(new Comparator<Flight>() {
+            @Override
+            public int compare(Flight flight1, Flight flight2) {
+                return Integer.compare(flight1.endTime(), flight2.endTime());
+            }
+        });
+
+        startMinPQ.addAll(flights);
+        endMinPQ.addAll(flights);
+        int allPassenger = 0;
+        while(startMinPQ.size() != 0){
+            if(startMinPQ.peek().startTime() <= endMinPQ.peek().endTime()){
+                allPassenger += startMinPQ.poll().passengers();
+                MaxNum = MaxNum > allPassenger ? MaxNum : allPassenger;
+            }else{
+                allPassenger -= endMinPQ.poll().passengers();
+            }
+        }
+
     }
 
     public int solve() {
         /* FIX ME */
-        return -1;
+        return MaxNum;
     }
 
 }
--- clab8/RabinKarpAlgorithm.java
+++ clab8/RabinKarpAlgorithm.java
@@ -6,6 +6,35 @@ public class RabinKarpAlgorithm {
      * This method will return -1 if no matching substring is found, or if the input is invalid.
      */
     public static int rabinKarp(String input, String pattern) {
+        int n = input.length();
+        int p = pattern.length();
+
+        if(p > n){
+            return -1;
+        }
+
+        StringBuilder stringBuilder = new StringBuilder();
+        for(int i = 0; i < p; i++){
+            stringBuilder.append(input.charAt(i));
+        }
+
+        RollingString rollInput = new RollingString(stringBuilder.toString(), p);
+        RollingString rollPattern = new RollingString(pattern, p);
+
+        int inputHash = rollInput.hashCode();
+        int patternHash = pattern.hashCode();
+
+        for(int s = 0; s < n - p; s++ ){
+            if(input.hashCode() == pattern.hashCode()){
+                if(rollInput.equals(rollPattern)){
+                    return s;
+                }
+            }
+            if(s < n - p){
+                rollInput.addChar(input.charAt(s + p));
+                inputHash = rollInput.hashCode();
+            }
+        }
         return -1;
     }
 
